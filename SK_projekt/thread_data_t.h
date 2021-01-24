9c05f6d35f1ff5541c1706474cd144d12bb92d9e
Marysia <maria.mikolajczakk@gmail.com>
2021-01-24 15:24:27 +0100

    Modyfikacja danych wątku

--- SK_projekt/thread_data_t.h
+++ SK_projekt/thread_data_t.h
@@ -1,12 +1,12 @@
 #include <pthread.h>
 
-
+#include "hashmap_threads.h"
 #ifndef SK_PROJEKT_THREAD_DATA_T_H
 #define SK_PROJEKT_THREAD_DATA_T_H
 struct thread_data_t
 {
     int fd;
-    pthread_mutex_t mutex;
+    struct table *mutex_table;
     int do_exit;
     int fd_file_transfer;
     char* file_transfer_address;
@@ -14,6 +14,5 @@ struct thread_data_t
     int transfer_mode;
     char* username;
     char working_directory[1035];
-    char* filename;
 };
 #endif //SK_PROJEKT_THREAD_DATA_T_H
